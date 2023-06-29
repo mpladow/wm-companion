fb6b0a16b3e8f9fd663c7f4d4197d5ddfb923db0
Michael <mpladow@gmail.com>
2023-06-29 13:40:04 +1000

    added additional counter

--- App.tsx
+++ App.tsx
@@ -1,13 +1,13 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeStack from "./src/Navigation/Home/HomeStack";
+import { StatusBar } from "expo-status-bar";
 
 const App = () => {
 	return (
 		<NavigationContainer>
 			<HomeStack />
+      <StatusBar hidden/>
 		</NavigationContainer>
 	);
 };
